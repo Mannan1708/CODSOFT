f4497ff97948f0355e3adf95009ae4711a4cb5c4
Mannan1708 <mannanagrawal17@gmail.com>
2024-07-11 12:51:07 +0530

    Task 2

---
+++ calculator_GUI.py
@@ -0,0 +1,64 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def addition(n1, n2):
+    return n1 + n2
+
+def subtraction(n1, n2):
+    return n1 - n2
+
+def multiplication(n1, n2):
+    return n1 * n2
+
+def division(n1, n2):
+    if n2 != 0:
+        return n1 / n2
+    else:
+        return "Cannot divide by zero"
+
+def calc(operations):
+    try:
+        n1 = float(first.get())
+        n2 = float(second.get())
+           
+        if operations == "+":
+            ans = addition(n1, n2)
+        elif operations == "-":
+            ans = subtraction(n1, n2)
+        elif operations == "*":
+            ans = multiplication(n1, n2)
+        elif operations == "/":
+            ans = division(n1, n2)
+        
+        res.config(text=f"Result: {ans}")
+    except ValueError:
+        messagebox.showerror("Enter valid numbers")
+
+start = tk.Tk()
+start.title("Simple Arithmetic Calculator")
+
+tk.Label(start, text="First number:").grid(row=0, column=0)
+tk.Label(start, text="Second number:").grid(row=1, column=0)
+
+first = tk.Entry(start)
+second = tk.Entry(start)
+
+first.grid(row=0, column=1)
+second.grid(row=1, column=1)
+
+button = tk.Button(start, text="+",command=lambda: calc("+"))
+button.grid(row=2, column=0)
+
+button = tk.Button(start, text="-",command=lambda: calc("-"))
+button.grid(row=2, column=1)
+
+button = tk.Button(start, text="*",command=lambda: calc("*"))
+button.grid(row=3, column=0)
+
+button = tk.Button(start, text="/",command=lambda: calc("/"))
+button.grid(row=3, column=1)
+
+res = tk.Label(start, text="Result: ")
+res.grid(row=6, column=1)
+
+start.mainloop()
